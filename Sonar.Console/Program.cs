ed183cca7bdce5e29aba9bd5f33af85138585af4
Tymur Mirzaiev <tymur.mirzaiev@care.com>
2021-09-30 12:40:02 +0300

    save

--- Sonar.Console/Program.cs
+++ Sonar.Console/Program.cs
@@ -7,6 +7,8 @@ namespace Sonar.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World1!");
+
         }
     }
 }
